2fa405287bb79d8200659b1db9097b2487e138d1
Tatsuki018 <76417684+Tatsuki018@users.noreply.github.com>
2020-12-21 13:53:51 +0900

    Add files via upload

---
+++ baba.java
@@ -0,0 +1,30 @@
+class baba {
+  int[] searching; //相手の点数
+  int avoidance; //逃げる点数
+  int[] a1, a2, a3, a4;　//船の配置
+
+  //searchのメソッド
+  public static void search() {
+
+  }
+
+  //avoidのメソッド
+  public static void avoid() {
+
+  }
+
+  //attackのメソッド
+  public static void attack() {
+
+  }
+
+  //runのメソッド
+  public static void run() {
+
+  }
+
+
+  public static void main(String[] args) {
+
+  }
+}
